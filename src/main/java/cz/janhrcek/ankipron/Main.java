0d62c072e9e6e571860979f9383bd814912979fd
Jan Hrcek <jhrcek@redhat.com>
2014-01-22 14:44:43 +0100

    Log error messages about note integrity violations

--- src/main/java/cz/janhrcek/ankipron/Main.java
+++ src/main/java/cz/janhrcek/ankipron/Main.java
@@ -14,11 +14,11 @@ public class Main {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
 
         //Use case 1: for words without pron, download the prons
-        //verifyAnkiNotesIntegrity();
+        verifyAnkiNotesIntegrity();
         //downloadMp3sForWords();
-        
+
         //Use case 2: having pron mp3s downloaded, add references to them to anki db
-        addMp3RefsToAnkiDb();
+        //addMp3RefsToAnkiDb();
     }
 
     public static void downloadMp3sForWords() throws IOException, ClassNotFoundException {
@@ -55,35 +55,36 @@ public class Main {
         List<AnkiNote> notesWithoutPron = new AnkiDatabase().getNotesWithoutPron();
         for (AnkiNote note : notesWithoutPron) {
             String deutsch = note.getDeutsch();
+            String articleError = "Note has flag %s, but does not start with %s: %s%n";
             //Notes with tag Femininum must have german field starting with e or r/e
             if (note.getTags().contains("Femininum")
                     && !(deutsch.startsWith("e ") || deutsch.startsWith("r/e "))) {
-                System.out.println(note);
+                System.err.printf(articleError, "Femininum", "e or r/e", note);
             }
 
             //Notes with tag Maskulinum must start with r or r/e or r/s
             if (note.getTags().contains("Maskulinum")
                     && !(deutsch.startsWith("r ") || deutsch.startsWith("r/e ")
                     || deutsch.startsWith("r/s "))) {
-                System.out.println(note);
+                System.err.printf(articleError, "Maskulinum", "r or r/e", note);
             }
 
             //Notes with tag Neutrum must start with s or (s)
             if (note.getTags().contains("Neutrum")
                     && !(deutsch.startsWith("s ") || deutsch.startsWith("(s) ")
                     || deutsch.startsWith("r/s "))) {
-                System.out.println(note);
+                System.err.printf(articleError, "Neutrum", "s or (s)", note);
             }
 
             //No nbsp; in notes!
             if (deutsch.contains("nbsp;")) {
-                System.out.println(note);
+                System.err.printf("Note contains nbsp; :%s%n", note);
             }
 
             //No special characters in words!
             String word = note.getWord();
             if (word != null && (word.contains("<") || word.contains(" ") || word.contains(">") || word.contains("­"))) {
-                System.out.println(note);
+                System.err.printf("Note contains one of characters <, ,>,-: %s%n", note);
             }
         }
     }
