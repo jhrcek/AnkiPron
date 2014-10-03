91005259b5f07118b8b0e2c9bb7ca670c4a48cb2
Jan Hrcek <jhrcek@redhat.com>
2014-10-03 14:53:44 +0200

    Simple command line client for happy + assembly configuration

---
+++ src/main/java/cz/janhrcek/ankipron/HappyClient.java
@@ -0,0 +1,67 @@
+package cz.janhrcek.ankipron;
+
+import cz.janhrcek.ankipron.search.DWDS;
+import cz.janhrcek.ankipron.search.Searcher;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class HappyClient {
+
+    public static void main(String[] args) {
+        Path infile = checkArgs(args);
+        List<String> words = readWords(infile);
+        Path downloadDir = getDownloadDir();
+
+        try (Searcher searcher = new DWDS(new FirefoxDriver())) {
+            Map<String, String> searchResult = searcher.batchSearch(words);
+
+            PronDownloader downloader = new PronDownloader(downloadDir);
+            downloader.performDownload(searchResult);
+        }
+    }
+
+    private static Path checkArgs(String[] args) {
+        if (args.length < 1) {
+            System.err.printf("ERROR: missing file argument. Usage:%njava -jar dwdown.jar <filename>%n");
+            System.exit(1);
+        }
+        Path infile = Paths.get(args[0]);
+        if (!Files.exists(infile)) {
+            System.err.printf("ERROR: file '%s' does not exist!%n", infile);
+            System.exit(1);
+        }
+        return infile;
+    }
+
+    private static List<String> readWords(Path infile) {
+        List<String> words = new ArrayList<>();
+        try (Scanner in = new Scanner(infile)) {
+            while (in.hasNextLine()) {
+                words.add(in.nextLine().trim());
+            }
+        } catch (IOException ioe) {
+            System.err.printf("ERROR: can't read words from file %s: %s%n", infile, ioe.getMessage());
+            System.exit(1);
+        }
+        return words;
+    }
+
+    private static Path getDownloadDir() {
+        Path dir = Paths.get("./downloads");
+        if (Files.notExists(dir)) {
+            try {
+                Files.createDirectory(dir);
+            } catch (IOException ex) {
+                System.err.println("ERROR: failed to create download dir");
+            }
+        }
+        return dir;
+    }
+}
