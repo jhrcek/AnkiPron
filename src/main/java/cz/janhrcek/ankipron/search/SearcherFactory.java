154ff963825a7040c2a5e68737b6fd16421bcf0a
Jan Hrček <jhrcek@redhat.com>
2016-06-16 19:23:35 +0200

    Add AdBlockPlus extension to chrome to speedup Duden search

--- src/main/java/cz/janhrcek/ankipron/search/SearcherFactory.java
+++ src/main/java/cz/janhrcek/ankipron/search/SearcherFactory.java
@@ -5,6 +5,8 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 /**
  *
@@ -13,24 +15,45 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class SearcherFactory {
 
     public static Searcher newDuden() {
-        return new Duden(createWebDriver());
+        return new Duden(createWebDriver(true));
     }
 
     public static Searcher newDwds() {
-        return new DWDS(createWebDriver());
+        return new DWDS(createWebDriver(false));
     }
 
     public static Searcher newSeznam() {
-        return new SeznamSlovnik(createWebDriver());
+        return new SeznamSlovnik(createWebDriver(false));
     }
 
-    private static WebDriver createWebDriver() {
+    private static WebDriver createWebDriver(boolean withAdBlock) {
         String currentDir = System.getProperty("user.dir");
         Path chromedriverBinary = Paths.get(currentDir, "chromedriver");
-        if (!Files.exists(chromedriverBinary)) {
-            throw new IllegalStateException("Expecting chromedriver binary to exist: " + chromedriverBinary.toAbsolutePath());
-        }
         
+        if (!Files.exists(chromedriverBinary)) {
+            throw new IllegalStateException("Expecting chromedriver binary to exist: " + chromedriverBinary.toAbsolutePath()
+                    + "\nYou can download it from https://sites.google.com/a/chromium.org/chromedriver/downloads");
+        }
+
+        if (withAdBlock) {
+            return createWebDriverWithAdBlock(currentDir);
+        }
+
         return new ChromeDriver();
+    }
+
+    private static WebDriver createWebDriverWithAdBlock(String currentDir) {
+        Path adBlockPlusFile = Paths.get(currentDir, "adblockpluschrome.crx");
+        
+        if (!Files.exists(adBlockPlusFile)) {
+            throw new IllegalStateException("Expecting AdBlockPlus extension file to exist: " + adBlockPlusFile.toAbsolutePath()
+                    + "\nYou can download it from https://downloads.adblockplus.org/devbuilds/adblockpluschrome/");
+        }
+
+        ChromeOptions options = new ChromeOptions();
+        options.addExtensions(adBlockPlusFile.toFile());
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+        return new ChromeDriver(capabilities);
     }
 }
