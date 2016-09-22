77348a953fea1d3ae978863de1d224a149910764
Jan Hrcek <jhrcek@redhat.com>
2016-09-22 12:02:12 +0200

    Update for new DWDS ui

--- src/main/java/cz/janhrcek/ankipron/search/DWDS.java
+++ src/main/java/cz/janhrcek/ankipron/search/DWDS.java
@@ -1,97 +1,58 @@
 package cz.janhrcek.ankipron.search;
 
-import java.util.List;
 import java.util.Objects;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebDriverException;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
  * This class encapsulates the use of a web page dwds.de
- *
  * @author jhrcek
  */
 public class DWDS extends AbstractSearcher {
 
-    private static final By SEARCH_INPUT = By.id("query_fast_search");
-    private static final By SEARCH_SUBMIT = By.id("dwds_main_search_submit");
-    private static final By PANEL_LOADING = By.cssSelector(".panel_loading");
-    private static final By PRONOUNCIATION = By.id("oneBitInsert_1");
-    private static final By WORD_FOUND = By.cssSelector(".wb_lzga");
-    private static final By PANEL_CLOSER = By.cssSelector(".panel_remove>img");
+    private static final By PRONUNCIATION = By.cssSelector("audio>source");
+    private static final By WORD_FOUND = By.xpath("//h2[contains(text(),'Bedeutung')]");
 
     public DWDS(WebDriver driver) {
         super(driver);
-        performInitialSearch();
-        closeUnwantedPanels();
     }
 
     @Override
     public SearchResult search(String word) {
-        Objects.requireNonNull(word, "aWord must not be null!");
+        Objects.requireNonNull(word, "word");
+        wait5seconds();
         pronUrl = null;
         System.out.print("Searching word " + ++counter + ": '" + word + "' - ");
-        driver.findElement(SEARCH_INPUT).clear();
-        driver.findElement(SEARCH_INPUT).sendKeys(word);
-        driver.findElement(SEARCH_SUBMIT).click();
-
+        driver.get("http://www.dwds.de/?qu=" + word);
         try {
-            waitForPageLoad();
             driver.findElement(WORD_FOUND);
             try {
-                driver.findElement(PRONOUNCIATION);
                 pronUrl = getAudioURL();
                 System.out.println("Pron URL: " + pronUrl);
                 return SearchResult.PRON_FOUND;
-            } catch (NoSuchElementException ex) { //Word in dictionary, but no pronounciation available
+            } catch (NoSuchElementException ex) { //Word in dictionary, but pronunciation not available
                 System.out.println("Pron NOT available");
                 return SearchResult.PRON_NOT_AVAILABLE;
             }
-        } catch (TimeoutException | NoSuchElementException ex) { //Word not in the dictionary
-            List<WebElement> notFoundElems = driver
-                    .findElements(By.xpath("//p[contains(text(),'Kein Eintrag vorhanden')]"));
-            if (!notFoundElems.isEmpty()) {
-                System.out.println(notFoundElems.get(0).getText());
-                return SearchResult.WORD_NOT_FOUND;
-            }
+        } catch (TimeoutException | NoSuchElementException ex) {
+            System.out.println("Word not found");
+            return SearchResult.WORD_NOT_FOUND;
         }
-        return SearchResult.UNKNOWN;
-    }
-
-    private void waitForPageLoad() {
-        new WebDriverWait(driver, 15, 100).until(
-                ExpectedConditions.not(ExpectedConditions.presenceOfAllElementsLocatedBy(PANEL_LOADING)));
     }
 
     private String getAudioURL() {
-        String flashvars = driver.findElement(PRONOUNCIATION).getAttribute("flashvars");
-        int mediaUrlBeginIndex = flashvars.indexOf("filename=") + "filename=".length();
-        return flashvars.substring(mediaUrlBeginIndex);
+        return driver.findElement(PRONUNCIATION).getAttribute("src");
     }
 
-    private void performInitialSearch() {
-        driver.get("http://www.dwds.de/?qu=und");
-        waitForPageLoad();
-    }
-
-    private void closeUnwantedPanels() {
-        List<WebElement> closeImages;
-        do {
-            closeImages = driver.findElements(PANEL_CLOSER);
-            try {
-                closeImages.get(1).click();
-            } catch (WebDriverException otherElementWouldReceiveClick) {
-                System.out.println("Ignoring exception on closing unwanted panel");
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException ignore2) {
-                }
-            }
-        } while (closeImages.size() > 2);
+    // Wait to avoid DWDS throttling repeated searches
+    private void wait5seconds() {
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            // ignore
+        }
     }
 }
--- src/test/java/cz/janhrcek/ankipron/search/DWDSTest.java
+++ src/test/java/cz/janhrcek/ankipron/search/DWDSTest.java
@@ -1,15 +1,12 @@
 package cz.janhrcek.ankipron.search;
 
+import static org.junit.Assert.*;
+
 import org.junit.AfterClass;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 /**
- *
  * @author jhrcek
  */
 public class DWDSTest {
@@ -18,42 +15,41 @@ public class DWDSTest {
 
     @Test
     public void wordWithoutPron() {
-        assertResutlAndPronURL("Ärger", SearchResult.PRON_NOT_AVAILABLE, false);
-        assertResutlAndPronURL("Konzentrationslager", SearchResult.PRON_NOT_AVAILABLE, false);
-        assertResutlAndPronURL("zweifellos", SearchResult.PRON_NOT_AVAILABLE, false);
+        assertResultAndPronURL("Ärger", SearchResult.PRON_NOT_AVAILABLE, false);
+        assertResultAndPronURL("Konzentrationslager", SearchResult.PRON_NOT_AVAILABLE, false);
+        assertResultAndPronURL("zweifellos", SearchResult.PRON_NOT_AVAILABLE, false);
     }
 
     @Test
     public void wordNotFound() {
-        assertResutlAndPronURL("nonexistent", SearchResult.WORD_NOT_FOUND, false);
-        assertResutlAndPronURL("abführend", SearchResult.WORD_NOT_FOUND, false);
-        assertResutlAndPronURL("Aufenthaltszimmer", SearchResult.WORD_NOT_FOUND, false);
-        assertResutlAndPronURL("letztendlich", SearchResult.WORD_NOT_FOUND, false);
+        assertResultAndPronURL("nonexistent", SearchResult.WORD_NOT_FOUND, false);
+        assertResultAndPronURL("Aufenthaltszimmer", SearchResult.WORD_NOT_FOUND, false);
+        assertResultAndPronURL("letztendlich", SearchResult.WORD_NOT_FOUND, false);
     }
 
     @Test
     public void pronFound() {
-        assertResutlAndPronURL("Bruder", SearchResult.PRON_FOUND, true);
-        assertResutlAndPronURL("und", SearchResult.PRON_FOUND, true);
-        assertResutlAndPronURL("Abscheulichkeit", SearchResult.PRON_FOUND, true);
+        assertResultAndPronURL("Bruder", SearchResult.PRON_FOUND, true);
+        assertResultAndPronURL("und", SearchResult.PRON_FOUND, true);
+        assertResultAndPronURL("Abscheulichkeit", SearchResult.PRON_FOUND, true);
     }
 
     @Test
-    public void withouPronAfterWithPron() {
-        assertResutlAndPronURL("und", SearchResult.PRON_FOUND, true);
+    public void withoutPronAfterWithPron() {
+        assertResultAndPronURL("und", SearchResult.PRON_FOUND, true);
         //Make sure pron Url is nulled after successfull search
-        assertResutlAndPronURL("Ärger", SearchResult.PRON_NOT_AVAILABLE, false);
+        assertResultAndPronURL("Ärger", SearchResult.PRON_NOT_AVAILABLE, false);
     }
 
     @Test
     public void notFoundAfterWithPron() {
-        assertResutlAndPronURL("und", SearchResult.PRON_FOUND, true);
+        assertResultAndPronURL("und", SearchResult.PRON_FOUND, true);
         //Make sure pron URL is nulled after successfull search
-        assertResutlAndPronURL("nonexistent", SearchResult.WORD_NOT_FOUND, false);
+        assertResultAndPronURL("nonexistent", SearchResult.WORD_NOT_FOUND, false);
     }
 
-    private void assertResutlAndPronURL(String word, SearchResult searchResult, boolean urlExpected) {
-        assertEquals(dwds.search(word), searchResult);
+    private void assertResultAndPronURL(String word, SearchResult searchResult, boolean urlExpected) {
+        assertEquals(searchResult, dwds.search(word));
         String url = dwds.getPronURL();
         if (urlExpected) {
             assertNotNull(url);
